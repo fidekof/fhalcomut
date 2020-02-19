648a699d1facc162992bdc5a3733c64a3e30360c
Fidel Lascano <fidekof@gmail.com>
2020-02-18 23:39:27 -0500

    Updates

---
+++ precache-manifest.e4f8cf95d9e9ca26ff6ec558bb7114a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee816b330b1dbba73f880a493eecafc",
+    "url": "/fhalcomut/index.html"
+  },
+  {
+    "revision": "923cdf12ec954654cf8c",
+    "url": "/fhalcomut/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d7c37b9a804137069b23",
+    "url": "/fhalcomut/static/js/2.cc0982c2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fhalcomut/static/js/2.cc0982c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923cdf12ec954654cf8c",
+    "url": "/fhalcomut/static/js/main.4734e9bf.chunk.js"
+  },
+  {
+    "revision": "283dda8e49205e054813",
+    "url": "/fhalcomut/static/js/runtime-main.7cece2f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fhalcomut/static/media/logo.5d5d9eef.svg"
+  }
+]);
